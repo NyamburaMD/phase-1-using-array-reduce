127fcffa95f6caec63287c95a4b36e9d510be5f8
darlenenyambura <darlene.nyambura@student.moringaschool.com>
2024-10-15 15:53:45 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+const batteryBatches = [4, 5, 3, 4, 4, 6, 5]; // Example array, make sure to use the provided one in index.js
 
-// Code your solution here
+const totalBatteries = batteryBatches.reduce((accumulator, currentValue) => {
+  return accumulator + currentValue;
+}, 0);
+
+console.log(totalBatteries); // This will log the total sum of all the batteries
+
